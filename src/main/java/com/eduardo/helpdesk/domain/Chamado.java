e2479ac94cfb38be7ed193d909ae21e40920d490
Eduardo Tadeu <eduardo.tsilva@hotmail.com>
2023-04-15 19:53:56 -0400

    atribuindo anotações JPA para as classes domain

--- src/main/java/com/eduardo/helpdesk/domain/Chamado.java
+++ src/main/java/com/eduardo/helpdesk/domain/Chamado.java
@@ -1,22 +1,41 @@
 package com.eduardo.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import com.eduardo.helpdesk.domain.enums.Prioridade;
 import com.eduardo.helpdesk.domain.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
-public class Chamado {
-
+@Entity
+public class Chamado implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
 	private Integer id;
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataAbertura = LocalDate.now();
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
 	private Prioridade prioridade;
 	private Status status;
 	private String titulo;
 	private String observacoes;
 
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
 	private Tecnico tecnico;
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 
 	public Chamado() {
